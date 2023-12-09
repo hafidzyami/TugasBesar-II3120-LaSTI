214a18625de361001ddf298bb9b8bd81ceb3198f
hafidzyami <18221074@std.stei.itb.ac.id>
2023-12-09 07:26:20 +0700

    fix image not loaded

--- lastiwebapp/next.config.js
+++ lastiwebapp/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains:['barcodeapi.org'],
         remotePatterns: [
             {
                 protocol: 'https',
