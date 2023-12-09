ca4509fd4a62caf087c867e4a5657fdb90edf482
hafidzyami <18221074@std.stei.itb.ac.id>
2023-12-09 07:32:29 +0700

    tes

--- lastiwebapp/next.config.js
+++ lastiwebapp/next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    i18n,
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'src/styles')],
-        prependData: `@import "variables.scss";`,
-    },
     images: {
         remotePatterns: [
             {
